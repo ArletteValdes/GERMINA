5c4c2c07ecb2a769d5454fbd0db0ea135c51a422
ArletteValdes <84408334+ArletteValdes@users.noreply.github.com>
2023-03-11 18:14:55 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import CssBaseline from '@mui/material/CssBaseline';
+import React from "react";import Cards from "./components/Cards";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import BasicPagination from "./components/Footer";
+
+
+function App() {
+    <>
+ <CssBaseline />
+    <Header/>
+    <Cards/>
+    <Footer/>
+    <BasicPagination/>
+
+</>
+}
+
+export default App
